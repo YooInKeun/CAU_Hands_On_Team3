6eb2096561f6a14226d05983fb9f74df985b24ee
YouuInKeun <keun0390@naver.com>
2019-11-26 14:27:20 +0900

    [ADD] 강의 데이터 프론트 return

--- GuideforOutsider/guideforoutsider/community/views.py
+++ GuideforOutsider/guideforoutsider/community/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import *
+from django.core.paginator import Paginator
 
 def lecture(request):
-    return render(request, 'community/lecture.html')
+    lectures = Lecture.objects.all()
+    return render(request, 'community/lecture.html', {'lectures' : lectures})
