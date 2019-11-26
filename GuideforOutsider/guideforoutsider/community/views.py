49a4f1762e2d2f8d2f442a028b5e19e76a22d317
YouuInKeun <keun0390@naver.com>
2019-11-26 15:34:24 +0900

    [ADD] 강의리스트 페이지네이션 완료

--- GuideforOutsider/guideforoutsider/community/views.py
+++ GuideforOutsider/guideforoutsider/community/views.py
@@ -3,5 +3,8 @@ from .models import *
 from django.core.paginator import Paginator
 
 def lecture(request):
-    lectures = Lecture.objects.all()
+    lectures = Lecture.objects.all().order_by('-id')
+    post_paginator = Paginator(lectures, 7)
+    page = request.GET.get('page')
+    lectures = post_paginator.get_page(page)
     return render(request, 'community/lecture.html', {'lectures' : lectures})
