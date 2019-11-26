19af361073da5799f558eddd25743bb54cb1ebf3
YouuInKeun <keun0390@naver.com>
2019-11-26 16:40:36 +0900

    [ADD] 검색 결과 기능 완료

--- GuideforOutsider/guideforoutsider/community/urls.py
+++ GuideforOutsider/guideforoutsider/community/urls.py
@@ -6,4 +6,5 @@ from . import views
 app_name = 'community'
 urlpatterns = [
     path('lecture/', views.lecture, name='lecture'),
+    path('lecture/query/', views.query_lecture, name='query_lecture'),
 ]
--- GuideforOutsider/guideforoutsider/community/views.py
+++ GuideforOutsider/guideforoutsider/community/views.py
@@ -8,3 +8,17 @@ def lecture(request):
     page = request.GET.get('page')
     lectures = post_paginator.get_page(page)
     return render(request, 'community/lecture.html', {'lectures' : lectures})
+
+def query_lecture(request):
+    if request.GET['category'] == '강의명':
+        lectures = Lecture.objects.filter(lecture_name__contains=request.GET['content']).order_by('-id')
+    elif request.GET['category'] == '교수명':
+        lectures = Lecture.objects.filter(professor_name=request.GET['content']).order_by('-id')
+    elif request.GET['category'] == '전공/교양':
+        lectures = Lecture.objects.filter(completion_division=request.GET['content']).order_by('-id')
+    elif request.GET['category'] == '학년':
+        lectures = Lecture.objects.filter(grade=request.GET['content']).order_by('-id')
+    
+    searched_category = request.GET['category']
+    searched_content = request.GET['content']
+    return render(request, 'community/lecture.html', {'lectures':lectures, 'searched_category':searched_category, 'searched_content':searched_content})
