57a32c8721a91734b22c2259df83cb2fa70ad26f
YouuInKeun <keun0390@naver.com>
2019-12-05 15:05:13 +0900

    [ADD] ver1

--- GuideforOutsider/guideforoutsider/community/urls.py
+++ GuideforOutsider/guideforoutsider/community/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('lecture/detail/<int:pk>/', views.lecture_detail, name='lecture_detail'),
     path('board/create/<int:pk>/', views.create_board, name='create_board'),
     path('board/create/complete/<int:pk>/', views.complete_create_board, name='complete_create_board'),
+    path('board/update/<int:pk>/', views.update_board, name='update_board'),
+    path('board/update/complete/<int:pk>/', views.complete_update_board, name='complete_update_board'),
+    path('board/delete/<int:pk>/', views.delete_board, name='delete_board'),
 ]
--- GuideforOutsider/guideforoutsider/community/views.py
+++ GuideforOutsider/guideforoutsider/community/views.py
@@ -25,10 +25,12 @@ def query_lecture(request):
 
 def lecture_detail(request, pk):
     lecture_pk = pk
+    lecture_name = Lecture.objects.get(pk=lecture_pk).lecture_name
+    
     studies = Board.objects.filter(board_category__board_category="스터디", lecture=pk)
     teamplays = Board.objects.filter(board_category__board_category="팀플", lecture=pk)
     reviews = Board.objects.filter(board_category__board_category="강의 후기", lecture=pk)
-    return render(request, 'community/lecture_detail.html', {'lecture_pk':lecture_pk, 'studies':studies, 'teamplays':teamplays, 'reviews':reviews})
+    return render(request, 'community/lecture_detail.html', {'lecture_name':lecture_name, 'lecture_pk':lecture_pk, 'studies':studies, 'teamplays':teamplays, 'reviews':reviews})
 
 def create_board(request, pk):
     lecture_pk = pk
@@ -36,6 +38,33 @@ def create_board(request, pk):
     return render(request, 'community/create_board.html', {'lecture_pk':lecture_pk, 'selected_lecture':selected_lecture})
 
 def complete_create_board(request, pk):
+    lecture_pk = pk
+    lecture_name = Lecture.objects.get(pk=lecture_pk).lecture_name
+
+    board = Board()
+    board.user = request.user
+    board.board_category = Board_Category.objects.get(board_category=request.POST['category'])
+    board.lecture = Lecture.objects.get(pk=lecture_pk)
+    board.title = request.POST['title']
+    board.content = request.POST['content']
+    board.save()
+
+    studies = Board.objects.filter(board_category__board_category="스터디", lecture=pk)
+    teamplays = Board.objects.filter(board_category__board_category="팀플", lecture=pk)
+    reviews = Board.objects.filter(board_category__board_category="강의 후기", lecture=pk)
+    return render(request, 'community/lecture_detail.html', {'lecture_name':lecture_name, 'lecture_pk':lecture_pk, 'studies':studies, 'teamplays':teamplays, 'reviews':reviews})
+
+def update_board(request, pk):
+    board_pk = pk
+    selected_board= Board.objects.filter(pk=board_pk)[0]
+    return render(request, 'community/update_board.html', {'board_pk':board_pk, 'selected_board':selected_board})
+
+def complete_update_board(request, pk):
+    lecture_pk = pk
+    selected_lecture= Lecture.objects.filter(pk=pk)[0]
+    return render(request, 'community/create_board.html', {'lecture_pk':lecture_pk, 'selected_lecture':selected_lecture})
+
+def delete_board(request, pk):
     lecture_pk = pk
     selected_lecture= Lecture.objects.filter(pk=pk)[0]
     return render(request, 'community/create_board.html', {'lecture_pk':lecture_pk, 'selected_lecture':selected_lecture})
